c55051e5c78207fe2fbcd0b3bf6a93eb69bd3b9d
tOñO <antvhell@hotmail.com>
2022-07-04 10:57:56 -0500

    first commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,12 @@
+import AppRouter from "./router/AppRouter";
+
+const JournalApp = () => {
+  return (
+    <>
+      {/* <h1>Journal</h1> */}
+      <AppRouter />
+    </>
+  );
+};
+
+export default JournalApp;
